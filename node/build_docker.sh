6ddaf56999ea28f2918f807571a1e48627236de8
Mario Kozjak <kozjakm1@gmail.com>
2016-10-05 11:39:35 +0200

    node: update default path to root directory
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- node/build_docker.sh
+++ node/build_docker.sh
@@ -2,23 +2,23 @@
 
 BASEDIR=$(dirname "$0")
 
-if ([[ ! -d $BASEDIR/../node_modules ]] || [[ ! `ls -A ./node_modules` ]]) && [ "$NODE_ENV" != "DEBUG" ]
+if ([[ ! -d $BASEDIR/../../node_modules ]] || [[ ! `ls -A ./node_modules` ]]) && [ "$NODE_ENV" != "DEBUG" ]
 then
 	npm install
 fi
 
-if ([[ ! -d $BASEDIR/../dist ]] || [[ ! `ls -A ./dist` ]]) && [ "$NODE_ENV" != "DEBUG" ]
+if ([[ ! -d $BASEDIR/../../dist ]] || [[ ! `ls -A ./dist` ]]) && [ "$NODE_ENV" != "DEBUG" ]
 then
-	mkdir -p $BASEDIR/../dist
+	mkdir -p $BASEDIR/../../dist
 	bash $BASEDIR/beautify_source.sh
-	$BASEDIR/../node_modules/.bin/eslint --fix -c ./.eslintrc.json './src/**/*.js' './test/**/*.js'
-	$BASEDIR/../node_modules/.bin/babel src -d $BASEDIR/../dist
+	$BASEDIR/../../node_modules/.bin/eslint --fix -c ./.eslintrc.json './src/**/*.js' './test/**/*.js'
+	$BASEDIR/../../node_modules/.bin/babel src -d $BASEDIR/../../dist
 fi
 
 if [ "$NODE_ENV" != "DEBUG" ]
 then
 	npm prune --production
-	docker build -f $BASEDIR/../docker/Dockerfile .
+	docker build -f $BASEDIR/../../docker/Dockerfile .
 else
-	docker build -f $BASEDIR/../docker/Dockerfile.debug .
+	docker build -f $BASEDIR/../../docker/Dockerfile.debug .
 fi
--- node/jsbeautify_source.sh
+++ node/jsbeautify_source.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-find $BASEDIR/../src $BASEDIR/../test -type f -name "*.js" -exec node ./node_modules/js-beautify/js/bin/js-beautify.js -r {} \;
+find $BASEDIR/../../src $BASEDIR/../../test -type f -name "*.js" -exec node ./node_modules/js-beautify/js/bin/js-beautify.js -r {} \;
